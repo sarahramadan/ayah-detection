6f4e4701da098a8ad6b9dbde2034c0cbe69b5cf8
Hossam Hammady <github@hammady.net>
2018-07-27 14:59:41 +0300

    Show a nice progress bar while doing fix_color_mode.py

--- fix_color_mode.py
+++ fix_color_mode.py
@@ -1,10 +1,12 @@
 import sys
 import glob
 from PIL import Image
+from tqdm import tqdm
 image_dir = sys.argv[1]
 print("Fixing color mode for PNG images in " + image_dir + "...")
-for file_name in glob.glob(image_dir + "/*.png"):
-  print(file_name)
-  image = Image.open(file_name).convert('RGBA')
-  image.save(file_name, "PNG");
-print("Done fixing color mode for PNG images in " + image_dir)
+for file_name in tqdm(glob.glob(image_dir + "/*.png")):
+  try:
+    image = Image.open(file_name).convert('RGBA')
+    image.save(file_name, "PNG")
+  except Exception as error:
+    print("Error in file: " + file_name + " [" + str(error) + "]")
