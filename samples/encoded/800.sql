933f0f4197b5cd1c45628bec2714e64ad7e519c4
Hossam Hammady <github@hammady.net>
2018-07-28 20:03:59 +0300

    Revamp sqlite_encoder.py to import, process and finally export

---
+++ samples/encoded/800.sql
@@ -0,0 +1,711 @@
+
+      INSERT INTO recitations_data (
+        recitation_id,
+        page_id,
+        line_id,
+        sura_id,
+        verse_id,
+        data
+      ) VALUES (2, 1, 1, 1, -1, 'NTI0Mjg5MDk=');
+
+      INSERT INTO recitations_data (
+        recitation_id,
+        page_id,
+        line_id,
+        sura_id,
+        verse_id,
+        data
+      ) VALUES (2, 1, 2, 1, 0, 'NDY4MjAzODY0Mjgz');
+
+      INSERT INTO recitations_data (
+        recitation_id,
+        page_id,
+        line_id,
+        sura_id,
+        verse_id,
+        data
+      ) VALUES (2, 1, 3, 1, 1, 'MjMzNjMzNjM3MTcyNTE0MDA=');
+
+      INSERT INTO recitations_data (
+        recitation_id,
+        page_id,
+        line_id,
+        sura_id,
+        verse_id,
+        data
+      ) VALUES (2, 1, 3, 1, 2, 'OTQwNjAzMjc3NjQwLDEwMjczOTc3NTMwMTA5MTc2Ng==');
+
+      INSERT INTO recitations_data (
+        recitation_id,
+        page_id,
+        line_id,
+        sura_id,
+        verse_id,
+        data
+      ) VALUES (2, 1, 4, 1, 3, 'MTQwODc3MzE5NDE2NiwyMTA4MjY2Mzg4MDQzODYzMzk=');
+
+      INSERT INTO recitations_data (
+        recitation_id,
+        page_id,
+        line_id,
+        sura_id,
+        verse_id,
+        data
+      ) VALUES (2, 1, 5, 1, 4, 'Mzg1NjM5NTMwNTQxMjI1MzE=');
+
+      INSERT INTO recitations_data (
+        recitation_id,
+        page_id,
+        line_id,
+        sura_id,
+        verse_id,
+        data
+      ) VALUES (2, 1, 5, 1, 5, 'MTg4MTIwNDY1NDYyNywxMTI4NzM4MTUwNjA1MTM0MjU=');
+
+      INSERT INTO recitations_data (
+        recitation_id,
+        page_id,
+        line_id,
+        sura_id,
+        verse_id,
+        data
+      ) VALUES (2, 1, 6, 1, 6, 'MjM0OTM3MzM5MTUwNSwxNDY5MzI3NTk2ODg5MDU0NzA=');
+
+      INSERT INTO recitations_data (
+        recitation_id,
+        page_id,
+        line_id,
+        sura_id,
+        verse_id,
+        data
+      ) VALUES (2, 1, 7, 1, 7, 'MjgyMTgyNzcyNDAzMCwzMjg5OTk3Mzc4NDEy');
+
+      INSERT INTO recitations_data (
+        recitation_id,
+        page_id,
+        line_id,
+        sura_id,
+        verse_id,
+        data
+      ) VALUES (2, 2, 1, 2, -1, 'NTI0Mjg5MTE=');
+
+      INSERT INTO recitations_data (
+        recitation_id,
+        page_id,
+        line_id,
+        sura_id,
+        verse_id,
+        data
+      ) VALUES (2, 2, 2, 2, 0, 'NDc2NzkzNzk4ODc4');
+
+      INSERT INTO recitations_data (
+        recitation_id,
+        page_id,
+        line_id,
+        sura_id,
+        verse_id,
+        data
+      ) VALUES (2, 2, 3, 2, 1, 'OTUzNTM1MTY4ODQ2LDE2MzUzODM5NjA0MDM5NzI0NQ==');
+
+      INSERT INTO recitations_data (
+        recitation_id,
+        page_id,
+        line_id,
+        sura_id,
+        verse_id,
+        data
+      ) VALUES (2, 2, 4, 2, 2, 'MTQzNDU1NzE1MzcyNSw3NDg3NDI1NTExNzkxMDU3Mg==');
+
+      INSERT INTO recitations_data (
+        recitation_id,
+        page_id,
+        line_id,
+        sura_id,
+        verse_id,
+        data
+      ) VALUES (2, 2, 5, 2, 3, 'MTkxMTI3Nzg3OTg1MiwyMzg4MDU0MjQ2MDQ0LDE0MzgzNjU4MjE4OTcyODUyMw==');
+
+      INSERT INTO recitations_data (
+        recitation_id,
+        page_id,
+        line_id,
+        sura_id,
+        verse_id,
+        data
+      ) VALUES (2, 2, 7, 2, 4, 'Mjg2OTA3MTY0MzQwMywzMzQ1ODMxOTUzMjc1');
+
+      INSERT INTO recitations_data (
+        recitation_id,
+        page_id,
+        line_id,
+        sura_id,
+        verse_id,
+        data
+      ) VALUES (2, 3, 1, 2, 5, 'NTI0Mjg4ODcsMTY3NDc3OTg0ODU3NDI0MDQ2');
+
+      INSERT INTO recitations_data (
+        recitation_id,
+        page_id,
+        line_id,
+        sura_id,
+        verse_id,
+        data
+      ) VALUES (2, 3, 2, 2, 6, 'MzczNzAxMTQ4ODQ2LDYwMjM2MzkyMzkyODE4OTQ5');
+
+      INSERT INTO recitations_data (
+        recitation_id,
+        page_id,
+        line_id,
+        sura_id,
+        verse_id,
+        data
+      ) VALUES (2, 3, 3, 2, 7, 'NzQ3MzM4MzM0NDY5LDExMjEwMzg4OTM0MDUsMjEzMDc4MDU2MzY1OTgyMTMy');
+
+      INSERT INTO recitations_data (
+        recitation_id,
+        page_id,
+        line_id,
+        sura_id,
+        verse_id,
+        data
+      ) VALUES (2, 3, 5, 2, 8, 'MTQ5ODk5MzE5NzQ5MiwxNDcyMTMyODU0Nzc4NDM0Njc=');
+
+      INSERT INTO recitations_data (
+        recitation_id,
+        page_id,
+        line_id,
+        sura_id,
+        verse_id,
+        data
+      ) VALUES (2, 3, 6, 2, 9, 'MTg3MjY0MDAxNjkwNyw2NDQ2MDAxNTk4NzA2NTQ0Mg==');
+
+      INSERT INTO recitations_data (
+        recitation_id,
+        page_id,
+        line_id,
+        sura_id,
+        verse_id,
+        data
+      ) VALUES (2, 3, 7, 2, 10, 'MjI0NjI4MjkwNDE2MiwzNjU5NDM2Njk1NDg2NTMzOA==');
+
+      INSERT INTO recitations_data (
+        recitation_id,
+        page_id,
+        line_id,
+        sura_id,
+        verse_id,
+        data
+      ) VALUES (2, 3, 8, 2, 11, 'MjYxOTkzODU3MDkzOCw2MTM2NDU0Mjg2MjUyNTIwMQ==');
+
+      INSERT INTO recitations_data (
+        recitation_id,
+        page_id,
+        line_id,
+        sura_id,
+        verse_id,
+        data
+      ) VALUES (2, 3, 9, 2, 12, 'Mjk5NzkwMTQ2MDI0MSwzMzcxNjAxNzU3MDMyLDQxMzgwNTY2ODQwMzc4MzAz');
+
+      INSERT INTO recitations_data (
+        recitation_id,
+        page_id,
+        line_id,
+        sura_id,
+        verse_id,
+        data
+      ) VALUES (2, 3, 11, 2, 13, 'Mzc0NTIyMTExNjg2Myw0MTE4OTI2MDY2NzExLDEwNzUyNzkzNzk4NjY1OTQzOA==');
+
+      INSERT INTO recitations_data (
+        recitation_id,
+        page_id,
+        line_id,
+        sura_id,
+        verse_id,
+        data
+      ) VALUES (2, 3, 13, 2, 14, 'NDQ5Njg1NTc5NDc5OCwxMjgzNTc0NTk5MjU0MDI4MjE=');
+
+      INSERT INTO recitations_data (
+        recitation_id,
+        page_id,
+        line_id,
+        sura_id,
+        verse_id,
+        data
+      ) VALUES (2, 3, 14, 2, 15, 'NDg3MDUyMjc5OTMwMSw1MjQ0MjA3NDk4NTI1');
+
+      INSERT INTO recitations_data (
+        recitation_id,
+        page_id,
+        line_id,
+        sura_id,
+        verse_id,
+        data
+      ) VALUES (2, 4, 1, 2, 16, 'NTI0Mjg4ODcsMzk5Njk4MjA0MDc0OTY4Nzg=');
+
+      INSERT INTO recitations_data (
+        recitation_id,
+        page_id,
+        line_id,
+        sura_id,
+        verse_id,
+        data
+      ) VALUES (2, 4, 2, 2, 17, 'MzczNjcxNDYxMDM4LDEzMDYwNTEzNjU3MDQ4Mjk0OQ==');
+
+      INSERT INTO recitations_data (
+        recitation_id,
+        page_id,
+        line_id,
+        sura_id,
+        verse_id,
+        data
+      ) VALUES (2, 4, 3, 2, 18, 'NzQ3MzU0NzE4NDY5LDExMjEwMzg4OTM0MDQsODE2MjkyMzc5NDcxMzg0ODM=');
+
+      INSERT INTO recitations_data (
+        recitation_id,
+        page_id,
+        line_id,
+        sura_id,
+        verse_id,
+        data
+      ) VALUES (2, 4, 5, 2, 19, 'MTQ5NDY2NzYyNDg4MywxODY4MzYzMjAzMDgyLDIyNDIwMjUzNTc5MjEsMTcwMDEzNTAxNjIwNzQ4OTg1');
+
+      INSERT INTO recitations_data (
+        recitation_id,
+        page_id,
+        line_id,
+        sura_id,
+        verse_id,
+        data
+      ) VALUES (2, 4, 8, 2, 20, 'MjYxNTY3NDY2NzcwNSw3NzEyNzEzNzI2MzM1NDY0MA==');
+
+      INSERT INTO recitations_data (
+        recitation_id,
+        page_id,
+        line_id,
+        sura_id,
+        verse_id,
+        data
+      ) VALUES (2, 4, 9, 2, 21, 'Mjk5MzYxMDE2Mjk2MCwzMzY3MzA2Nzg5NzM1LDM3NDA5Njg5NDQ1NzQsMTgxODM2OTQ5NTg2MTgzMTg5');
+
+      INSERT INTO recitations_data (
+        recitation_id,
+        page_id,
+        line_id,
+        sura_id,
+        verse_id,
+        data
+      ) VALUES (2, 4, 12, 2, 22, 'NDExNDYyMTAwNjg2OSw0NDg4MjkzMjU0MjUyLDE0NDY4Mjk5OTk4NDY4NjI3NQ==');
+
+      INSERT INTO recitations_data (
+        recitation_id,
+        page_id,
+        line_id,
+        sura_id,
+        verse_id,
+        data
+      ) VALUES (2, 4, 14, 2, 23, 'NDg2MTkzNjY2NTc5NSw1MjM1NjE3NTYzOTMx');
+
+      INSERT INTO recitations_data (
+        recitation_id,
+        page_id,
+        line_id,
+        sura_id,
+        verse_id,
+        data
+      ) VALUES (2, 5, 1, 2, 24, 'NTI0Mjg4ODcsMzczNzE0NTgzNzI3LDc1MTY3MTcwNTg2MiwxNDkxOTI5OTA5OTUyNTQ3MA==');
+
+      INSERT INTO recitations_data (
+        recitation_id,
+        page_id,
+        line_id,
+        sura_id,
+        verse_id,
+        data
+      ) VALUES (2, 5, 4, 2, 25, 'MTEyNTI4NDkwNTMxMCwxNTAzMjkwOTgyODM4LDE4ODEyNDgxMDQ5NzMsMjI1NDkxMDI1OTgxMywyNjMyODY3MzgxOTQ4LDEzMzE0MDY3MDUxMzY3NzA3Ng==');
+
+      INSERT INTO recitations_data (
+        recitation_id,
+        page_id,
+        line_id,
+        sura_id,
+        verse_id,
+        data
+      ) VALUES (2, 5, 9, 2, 26, 'MzAwNjUwODEwNjUxNiwzMzg0NDg2NjU4OTI0LDM3NjI0NDM3ODEwNTk=');
+
+      INSERT INTO recitations_data (
+        recitation_id,
+        page_id,
+        line_id,
+        sura_id,
+        verse_id,
+        data
+      ) VALUES (2, 5, 12, 2, 27, 'NDEzNjEwNTkzNTg5OSw0OTgyNTU4NDk0MDg0NDE0Ng==');
+
+      INSERT INTO recitations_data (
+        recitation_id,
+        page_id,
+        line_id,
+        sura_id,
+        verse_id,
+        data
+      ) VALUES (2, 5, 13, 2, 28, 'NDUxNDAyMjIyOTEwNiw0ODg3NzI1MjEyODc0LDUyNjU2ODIzMzUwMDk=');
+
+      INSERT INTO recitations_data (
+        recitation_id,
+        page_id,
+        line_id,
+        sura_id,
+        verse_id,
+        data
+      ) VALUES (2, 6, 1, 2, 29, 'NTI0Mjg4ODksMzgyMzA0NTE4MzIyLDIyNTE4NzYyNjkzNDYwMjM1');
+
+      INSERT INTO recitations_data (
+        recitation_id,
+        page_id,
+        line_id,
+        sura_id,
+        verse_id,
+        data
+      ) VALUES (2, 6, 3, 2, 30, 'NzY0NTA5NDIxODM1LDExNDY4MDg2OTcxODgsOTM0NTEyMjEzMjg3MjQ0MTM=');
+
+      INSERT INTO recitations_data (
+        recitation_id,
+        page_id,
+        line_id,
+        sura_id,
+        verse_id,
+        data
+      ) VALUES (2, 6, 5, 2, 31, 'MTUyOTAzMDExNTc3Myw2ODY4MTgwNTYzMDI3NjExOA==');
+
+      INSERT INTO recitations_data (
+        recitation_id,
+        page_id,
+        line_id,
+        sura_id,
+        verse_id,
+        data
+      ) VALUES (2, 6, 6, 2, 32, 'MTkxMTI3NjQzODAzOCwyMjkzNTY0OTY1NDg3LDI2NzU4MTcwNTQ5MjAsMjEzMDc5NjE1NDM5MTEwOTQ1');
+
+      INSERT INTO recitations_data (
+        recitation_id,
+        page_id,
+        line_id,
+        sura_id,
+        verse_id,
+        data
+      ) VALUES (2, 6, 9, 2, 33, 'MzA1ODA2NjMyNjMwNSw5NjgzMDgzMjMwOTY5OTQ1MA==');
+
+      INSERT INTO recitations_data (
+        recitation_id,
+        page_id,
+        line_id,
+        sura_id,
+        verse_id,
+        data
+      ) VALUES (2, 6, 10, 2, 34, 'MzQ0MDI5MTM0OTM3MCwzODIyNTczMzIzMjE5LDcxMjE3MzczOTMzMjA4NjIw');
+
+      INSERT INTO recitations_data (
+        recitation_id,
+        page_id,
+        line_id,
+        sura_id,
+        verse_id,
+        data
+      ) VALUES (2, 6, 12, 2, 35, 'NDIwNDc4OTU2NDQ2MCw0NTg3MDc3NTAyMDg1LDQzMzUyMTE1NzQzMDMyNTQy');
+
+      INSERT INTO recitations_data (
+        recitation_id,
+        page_id,
+        line_id,
+        sura_id,
+        verse_id,
+        data
+      ) VALUES (2, 6, 14, 2, 36, 'NDk2OTI4NzI1NTI2Miw1MzUxNTgxNjgwOTUx');
+
+      INSERT INTO recitations_data (
+        recitation_id,
+        page_id,
+        line_id,
+        sura_id,
+        verse_id,
+        data
+      ) VALUES (2, 7, 1, 2, 37, 'NTI0Mjg4ODgsODg2NjQ5OTU2NzM0MDc2NjQ=');
+
+      INSERT INTO recitations_data (
+        recitation_id,
+        page_id,
+        line_id,
+        sura_id,
+        verse_id,
+        data
+      ) VALUES (2, 7, 2, 2, 38, 'Mzc3OTc3NzY2MDY0LDIwMjY2OTU0Mjg5ODQwMzky');
+
+      INSERT INTO recitations_data (
+        recitation_id,
+        page_id,
+        line_id,
+        sura_id,
+        verse_id,
+        data
+      ) VALUES (2, 7, 3, 2, 39, 'NzU1OTE4OTYyOTUyLDExMzM5MjM3OTUyOTYsOTkzNjIxNzg2NTk3NzkwMDA=');
+
+      INSERT INTO recitations_data (
+        recitation_id,
+        page_id,
+        line_id,
+        sura_id,
+        verse_id,
+        data
+      ) VALUES (2, 7, 5, 2, 40, 'MTUxMTg1MTYyMjg0MCwxODg5ODM4MDM5NTY4LDEzNDI2NTgzMTY4NjE0NDYxNg==');
+
+      INSERT INTO recitations_data (
+        recitation_id,
+        page_id,
+        line_id,
+        sura_id,
+        verse_id,
+        data
+      ) VALUES (2, 7, 7, 2, 41, 'MjI2Nzc3Mzk5MzU3NiwxMjgwNzM3NjAxNTU2MzIzMjA=');
+
+      INSERT INTO recitations_data (
+        recitation_id,
+        page_id,
+        line_id,
+        sura_id,
+        verse_id,
+        data
+      ) VALUES (2, 7, 8, 2, 42, 'MjY0NTcyOTY3MzkyMCwxMjMyODkwNjM1MDg2NzMzMDQ=');
+
+      INSERT INTO recitations_data (
+        recitation_id,
+        page_id,
+        line_id,
+        sura_id,
+        verse_id,
+        data
+      ) VALUES (2, 7, 9, 2, 43, 'MzAyMzY4NTY4MTk0NCw0MjIyNDY0ODE3MzEyNjUxMg==');
+
+      INSERT INTO recitations_data (
+        recitation_id,
+        page_id,
+        line_id,
+        sura_id,
+        verse_id,
+        data
+      ) VALUES (2, 7, 10, 2, 44, 'MzQwMTYyMzkyOTcxMiwzNzc5NjIzNjUwMjQ4');
+
+      INSERT INTO recitations_data (
+        recitation_id,
+        page_id,
+        line_id,
+        sura_id,
+        verse_id,
+        data
+      ) VALUES (2, 7, 12, 2, 45, 'MTk0MjU5MzA5NzM4MDc2NDg=');
+
+      INSERT INTO recitations_data (
+        recitation_id,
+        page_id,
+        line_id,
+        sura_id,
+        verse_id,
+        data
+      ) VALUES (2, 7, 12, 2, 46, 'NDE1NzUzMjg2NTU2OCw0NTM1NTM3ODk0NTIwLDE3OTg2NzQyMzYxMzEyNTg0MA==');
+
+      INSERT INTO recitations_data (
+        recitation_id,
+        page_id,
+        line_id,
+        sura_id,
+        verse_id,
+        data
+      ) VALUES (2, 7, 14, 2, 47, 'NDkxMzQ4NDQ2NTM2MCw1MjkxNDUyMTM4Nzkz');
+
+      INSERT INTO recitations_data (
+        recitation_id,
+        page_id,
+        line_id,
+        sura_id,
+        verse_id,
+        data
+      ) VALUES (2, 8, 1, 2, 48, 'NTI0Mjg4ODcsMzczNzE0NTgzNzI2LDE2MDE2MDAwOTEyNTEwMTgzMA==');
+
+      INSERT INTO recitations_data (
+        recitation_id,
+        page_id,
+        line_id,
+        sura_id,
+        verse_id,
+        data
+      ) VALUES (2, 8, 3, 2, 49, 'NzQ3MzYxNTk5NzUwLDEwMTg5NTA2NjkwMzExODE3Mw==');
+
+      INSERT INTO recitations_data (
+        recitation_id,
+        page_id,
+        line_id,
+        sura_id,
+        verse_id,
+        data
+      ) VALUES (2, 8, 4, 2, 50, 'MTEyNTMwNTE1NTkzMywxMjk0OTM0NzkyNDcwNTcxNg==');
+
+      INSERT INTO recitations_data (
+        recitation_id,
+        page_id,
+        line_id,
+        sura_id,
+        verse_id,
+        data
+      ) VALUES (2, 8, 5, 2, 51, 'MTQ5ODk0NjYwMTM5Niw0MTM3ODY5NDIzNDYzNjgxMg==');
+
+      INSERT INTO recitations_data (
+        recitation_id,
+        page_id,
+        line_id,
+        sura_id,
+        verse_id,
+        data
+      ) VALUES (2, 8, 6, 2, 52, 'MTg3MjYxNTM3NTM3MiwzNjU5Mzk5NzU4NzY3Nzc5NQ==');
+
+      INSERT INTO recitations_data (
+        recitation_id,
+        page_id,
+        line_id,
+        sura_id,
+        verse_id,
+        data
+      ) VALUES (2, 8, 7, 2, 53, 'MjI1MDU3MTM4MzM5NSwyNjI0Mjc3NDQ3MzU0LDI5OTc5Mzk2MDIxOTQsMzM3NTg5NjcyNDMyOSwyMTMzNjE3ODE5MDU1NTY0MTY=');
+
+      INSERT INTO recitations_data (
+        recitation_id,
+        page_id,
+        line_id,
+        sura_id,
+        verse_id,
+        data
+      ) VALUES (2, 8, 11, 2, 54, 'Mzc0OTU1NjEyNjY1Niw1MTUxNDA0Mzk1OTA4NDA1Ng==');
+
+      INSERT INTO recitations_data (
+        recitation_id,
+        page_id,
+        line_id,
+        sura_id,
+        verse_id,
+        data
+      ) VALUES (2, 8, 12, 2, 55, 'NDEyMzE4MDU5ODI5Niw4NTg1NDM2OTA3NDkwNjIyMw==');
+
+      INSERT INTO recitations_data (
+        recitation_id,
+        page_id,
+        line_id,
+        sura_id,
+        verse_id,
+        data
+      ) VALUES (2, 8, 13, 2, 56, 'NDUwMTE0NTcxNTgyMyw0ODc0ODQwMzEwOTgyLDUyNDg1MDI0NjU4MjE=');
+
+      INSERT INTO recitations_data (
+        recitation_id,
+        page_id,
+        line_id,
+        sura_id,
+        verse_id,
+        data
+      ) VALUES (2, 9, 1, 2, 57, 'NTI0Mjg4ODcsMzczNzE0NTgzNzI3LDg1NTY5MTQ0NTkxNzQ1Mjg2');
+
+      INSERT INTO recitations_data (
+        recitation_id,
+        page_id,
+        line_id,
+        sura_id,
+        verse_id,
+        data
+      ) VALUES (2, 9, 3, 2, 58, 'NzUxNjM5MjAwMDA2LDExMjUzMzM4NjA3MDIsNjMzMzMzNzMwNTA4ODA0Mzg=');
+
+      INSERT INTO recitations_data (
+        recitation_id,
+        page_id,
+        line_id,
+        sura_id,
+        verse_id,
+        data
+      ) VALUES (2, 9, 5, 2, 59, 'MTUwMzI1MzI5OTYzOCwxODgxMjQ4MTA0OTczLDIyNTQ5MTAyNTk4MTMsMjYzMjg2NzM4MTk0OCwyMTMzNjEwMzg4NzYyMTQwMzY=');
+
+      INSERT INTO recitations_data (
+        recitation_id,
+        page_id,
+        line_id,
+        sura_id,
+        verse_id,
+        data
+      ) VALUES (2, 9, 9, 2, 60, 'MzAwNjUyNjc4NDI3NiwzMzg0NDg2NjU4OTI0LDM3NjI0NDM3ODEwNTksNDEzNjEwNTkzNTg5OSw0NTE0MDYzMDU4MDM0LDQ4ODc3MjUyMTI4NzQsNTI2NTY4MjMzNTAwOQ==');
+
+      INSERT INTO recitations_data (
+        recitation_id,
+        page_id,
+        line_id,
+        sura_id,
+        verse_id,
+        data
+      ) VALUES (2, 10, 1, 2, 61, 'NTI0Mjg4ODcsMzczNzE0NTgzNzI3LDE3NzMzNjc1MjA0NDc3MTkx');
+
+      INSERT INTO recitations_data (
+        recitation_id,
+        page_id,
+        line_id,
+        sura_id,
+        verse_id,
+        data
+      ) VALUES (2, 10, 3, 2, 62, 'NzUxNjIzNDA1ODMxLDExMjk2Mjg4Mjc5OTksNTc0MjI0MDI4MzQ5MjM5NTk=');
+
+      INSERT INTO recitations_data (
+        recitation_id,
+        page_id,
+        line_id,
+        sura_id,
+        verse_id,
+        data
+      ) VALUES (2, 10, 5, 2, 63, 'MTUwNzU0Njg5MDY3OSwxODg1NTQzMDcyMjcxLDE2OTQ1MDE5OTQ4MDAwOTMxOQ==');
+
+      INSERT INTO recitations_data (
+        recitation_id,
+        page_id,
+        line_id,
+        sura_id,
+        verse_id,
+        data
+      ) VALUES (2, 10, 7, 2, 64, 'MjI2MzQ4NzIxODI3OSw3Nzk3MTIxMDAwNjE2ODI1NQ==');
+
+      INSERT INTO recitations_data (
+        recitation_id,
+        page_id,
+        line_id,
+        sura_id,
+        verse_id,
+        data
+      ) VALUES (2, 10, 8, 2, 65, 'MjY0MTQyMzA0MTIxNSwzOTk3MjQ2NjEwNzM1MTgzMQ==');
+
+      INSERT INTO recitations_data (
+        recitation_id,
+        page_id,
+        line_id,
+        sura_id,
+        verse_id,
+        data
+      ) VALUES (2, 10, 9, 2, 66, 'MzAxOTM3MTMxNTk5MSwzMzk3MzcxNTYwODE1LDM3NzUzMjg2ODI5NTEsMjEzMDgwNzEwNjU1NzcxNjc5');
+
+      INSERT INTO recitations_data (
+        recitation_id,
+        page_id,
+        line_id,
+        sura_id,
+        verse_id,
+        data
+      ) VALUES (2, 10, 12, 2, 67, 'NDE1MzI4Mjk4NzAzOSw0NTMxMjQyOTI3MjIzLDE3Njc3MTE5NDU3NDM0MTMyNw==');
+
+      INSERT INTO recitations_data (
+        recitation_id,
+        page_id,
+        line_id,
+        sura_id,
+        verse_id,
+        data
+      ) VALUES (2, 10, 14, 2, 68, 'NDkwOTE4ODc3NzE2Nyw1Mjg3MTU3MTcxNDk1');
--- sqlite_encoder.py
+++ sqlite_encoder.py
@@ -1,37 +1,177 @@
 import sys
 import sqlite3
 import base64
+import argparse
+from tempfile import mkstemp
+from glob import glob
+import csv
+from tqdm import tqdm
+from utils import safe_makedir
 
-ratio = float(sys.argv[1]) / 800
-rec = "2"
 
-conn = sqlite3.connect('/home/ayman/phpliteAdmin/Tadween')
+def parse_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--input_path', type=str, required=True,
+                        help='''Path to input folder containing segmented data SQL files to import''')
+    parser.add_argument('--output_path', type=str, required=True,
+                        help='''Path to output folder to generate platform specific files into''')
+    parser.add_argument('--reference_width', type=float, required=True,
+                        help='''Reference width of which segmented data SQL files were generated''')
+    parser.add_argument('--recitation_id', type=int, required=True,
+                        help='''Recitation ID of the segmented data''')
+    return parser.parse_args()
 
-cursor = conn.cursor()
-st = ""
-for row in cursor.execute('select * from encoded_glyphs order by pageid, suraid, verseid'):
-	data = row[5]
-	data = data.replace('[', '').replace(']', '')
-	rects = data.split('","')
-	data = ""
-	for rect in rects:
-		coords = rect.replace('"', '')
-		coords = coords.split(',')
-		if len(data) > 0:
-			data += ","
-		left = int(round(int(coords[0]) * ratio))
-		top = int(round(int(coords[1]) * ratio))
-		right = int(round(int(coords[2]) * ratio))
-		bottom = int(round(int(coords[3]) * ratio))
-		data += str(left << 48 | top << 32 | right << 16 | bottom)
-	data = base64.b64encode(data)
-	if len(st) > 0:
-		st += "\n"
-	#st += "insert into recitations_data (recitation_id, page_id, sura_id, verse_id, line_id, data) values (%s,%s,%s,%s,%s,'%s')" % (rec, row[1], row[3], row[4], row[2], data)
-	#conn.cursor().execute('update encoded_glyphs set encoded_data="%s" where id = %d' % (data, row[0]))
 
-print st
+def create_db():
+  print("Creating database...")
+  _, db_file_name = mkstemp()
+  print("Created database at " + db_file_name)
+  conn = sqlite3.connect(db_file_name)
+  cursor = conn.cursor()
+  cursor.execute('''
+    CREATE TABLE "glyphs" (
+      'id' INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+      'pageId' INTEGER,
+      'lineId' INTEGER,
+      'suraId' INTEGER,
+      'verseId' INTEGER,
+      'indexId' INTEGER,
+      'left' INTEGER,
+      'right' INTEGER,
+      'top' INTEGER,
+      'bottom' INTEGER);
+  ''')
+  cursor.execute('''
+    CREATE TABLE "encoded_glyphs" (
+      'id' INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+      'pageId' INTEGER,
+      'lineId' INTEGER,
+      'suraId' INTEGER,
+      'verseId' INTEGER,
+      'data' TEXT);
+  ''')
+  return conn
 
-conn.commit()
 
-conn.close()
+def import_data(conn, sql_dir):
+  print("Importing SQL files in " + sql_dir + "...")
+  cursor = conn.cursor()
+  for file_name in tqdm(glob(sql_dir + "/*.sql")):
+    with open(file_name, "rb") as file:
+      cursor.executescript(file.read())
+  conn.commit()
+
+
+def group_data(conn):
+  print("Grouping data...")
+  cursor = conn.cursor()
+  cursor.execute('''
+    INSERT INTO encoded_glyphs (
+      pageId,
+      lineId,
+      suraId,
+      verseId,
+      data
+    )
+    SELECT
+      pageId,
+      min(lineId) lineId,
+      suraId,
+      verseId,
+      '[' || group_concat('"' || left || ',' || top || ',' || right || ',' || bottom || '"', ',') || ']' data
+    FROM glyphs
+    GROUP BY
+      suraid,
+      verseid,
+      pageid
+    ORDER BY
+      suraid,
+      verseid,
+      pageid;
+  ''')
+  conn.commit()
+
+
+def encode_data(conn, width, ref_width):
+  encoded_data = []
+  ratio = width / ref_width
+  cursor = conn.cursor()
+  for row in cursor.execute('SELECT * FROM encoded_glyphs ORDER BY pageid, suraid, verseid'):
+    data = row[5]
+    data = data.replace('[', '').replace(']', '')
+    rects = data.split('","')
+    data = ""
+    for rect in rects:
+      coords = rect.replace('"', '')
+      coords = coords.split(',')
+      if len(data) > 0:
+        data += ","
+      left = int(round(int(coords[0]) * ratio))
+      top = int(round(int(coords[1]) * ratio))
+      right = int(round(int(coords[2]) * ratio))
+      bottom = int(round(int(coords[3]) * ratio))
+      data += str(left << 48 | top << 32 | right << 16 | bottom)
+    data = base64.b64encode(data)
+    encoded_data.append((row[1], row[2], row[3], row[4], data))
+  return encoded_data
+
+
+def export_data_sql(data, recitation_id, file_name):
+  print("Exporting %d records to %s..." % (len(data), file_name))
+  with open(file_name, 'wb') as file:
+    for row in data:
+      file.write('''
+      INSERT INTO recitations_data (
+        recitation_id,
+        page_id,
+        line_id,
+        sura_id,
+        verse_id,
+        data
+      ) VALUES (%s, %s, %s, %s, %s, '%s');
+      ''' \
+      % (recitation_id, row[0], row[1], row[2], row[3], row[4]))
+
+
+def export_data_tsv(data, recitation_id, file_name):
+  print("Exporting %d records to %s..." % (len(data), file_name))
+  with open(file_name, 'wb') as file:
+    writer = csv.writer(file, delimiter='\t',
+                            quotechar='"', quoting=csv.QUOTE_MINIMAL)
+    writer.writerow([
+      'rewayaId',
+      'pageId',
+      'suraId',
+      'verseId',
+      'lineId',
+      'data'
+    ])
+    for row in data:
+      writer.writerow([
+        recitation_id,
+        row[0],
+        row[2],
+        row[3],
+        row[1],
+        row[4]
+      ])
+
+
+if __name__ == "__main__":
+  args = parse_arguments()
+
+  conn = create_db()
+
+  import_data(conn, args.input_path)
+  group_data(conn)
+
+  out_dir = safe_makedir(args.output_path)
+  for width in [320, 480, 800, 1200, 1500]:
+    data = encode_data(conn, width, args.reference_width)
+    file_name = "%s/%d.sql" % (out_dir, width)
+    export_data_sql(data, args.recitation_id, file_name)
+    if width == 800:
+      file_name = "%s/%d.tsv" % (out_dir, width)
+      export_data_tsv(data, args.recitation_id, file_name)
+
+  conn.close()
