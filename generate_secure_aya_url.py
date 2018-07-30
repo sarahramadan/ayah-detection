2ba4dbb91ec1cbc0889e68156b7068b6ac327bd5
Hossam Hammady <github@hammady.net>
2018-07-30 16:38:28 +0300

    Add script to generate secure audio url given correct salt
    Environment variable ELMOHAFEZ_SALT must be set before calling
    the script.
    For usage, run `generate_secure_aya_url.py -h`

---
+++ generate_secure_aya_url.py
@@ -0,0 +1,27 @@
+import os
+import sys
+import argparse
+from md5 import md5
+
+def parse_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--recitation_id', type=int, required=True)
+    parser.add_argument('--qaree_id', type=int, required=True)
+    parser.add_argument('--sura_id', type=int, required=True)
+    parser.add_argument('--aya_id', type=int, required=True)
+    return parser.parse_args()
+
+
+if __name__ == "__main__":
+  args = parse_arguments()
+
+  base_url = "http://audio.elmohafez.com"
+  try:
+    salt = os.environ["ELMOHAFEZ_SALT"]
+  except KeyError as error:
+    print("Environment variable ELMOHAFEZ_SALT must be set")
+    sys.exit(1)
+
+  input = "%d-%d-%d-%d0%s" % (args.recitation_id, args.qaree_id, args.sura_id, args.aya_id, salt)
+  hash = md5(input).hexdigest()
+  print("%s/%d-%d/%c/%s.mp3" % (base_url, args.recitation_id, args.qaree_id, hash[0], hash))
