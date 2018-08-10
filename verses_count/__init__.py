0da26c994899e0049ab28d04b64ebf25c98fe94a
Hossam Hammady <github@hammady.net>
2018-08-10 13:21:27 +0300

    Separate counting methods in files for easy manipulation
    In each file, each line number corresponds to sura number.
    This makes it easy to find any sura aya count by jumping to its line.
    It also makes git commits easy to understand as the changed sura will
    be the only one highlighted in diff, not the whole one-line array.

---
+++ verses_count/__init__.py
@@ -0,0 +1,16 @@
+from basry import basry
+from kofy import kofy
+from madany1 import madany1
+from madany2 import madany2
+from makky import makky
+from shamy import shamy
+
+
+verses_count = {
+  "basry": basry,
+  "kofy": kofy,
+  "madany1": madany1,
+  "madany2": madany2,
+  "makky": makky,
+  "shamy": shamy
+}
